90b3e28035d82acb01d7005cf66334aa70e5f86a
Víctor Pérez Figueras <vperez-f@student.42barcelona.com>
2025-05-23 02:51:42 +0200

    Update script.js

--- srcs/requirements/bonus/static_web/script.js
+++ srcs/requirements/bonus/static_web/script.js
@@ -1,14 +1,15 @@
 const title = document.getElementById("title");
+const subtitle = document.getElementById("subtitle");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 class Snake {
 	constructor() {
 		this.size = 1;
-		this.body = [{x: 50, y: 50}];
+		this.body = [{x: (canvas.width / 2) - bodySize, y: (canvas.height / 2)}];
 		this.head = this.body[0];
 		this.direction = {x: 0, y: 0};
-		this.speed = 0.5;
+		this.speed = 1;
 	}
 	grow(last) {
 		this.body.push({x: last.x, y: last.y});
@@ -18,10 +19,12 @@ class Snake {
 
 var		inGame = false;
 var		moving = false;
+const	targetTime = 1000 / 30;
+var		lastTime = performance.now();
 const	bodySize = 10;
-const	bodyColor = "green";
-const	headColor = "white";
-const	foodSize = 5;
+const	bodyColor = "#94b19e";
+const	headColor = "#79dc1d";
+const	foodSize = 10;
 const	foodColor = "red";
 var		food = [];
 
@@ -40,6 +43,7 @@ window.addEventListener("keydown", (e) => {
 		inGame = true;
 		canvas.focus();
 		title.style.opacity = 0;
+		subtitle.style.opacity = 0;
 		setTimeout(moveTitle, 300);
 		setTimeout(startGame, 300);
 	}
@@ -70,8 +74,8 @@ canvas.addEventListener("keydown", (e) => {
 
 function	populateFood(amount) {
 	for (let i = 0; i < amount; ++i) {
-		let x = Math.floor(Math.random() * 300);
-		let y = Math.floor(Math.random() * 300);
+		let x = Math.floor(Math.random() * canvas.width);
+		let y = Math.floor(Math.random() * canvas.height);
 		let newMeal = {x: x, y: y};
 		food.push(newMeal);
 	}
@@ -83,6 +87,8 @@ function	startGame() {
 	}
 	else {
 		populateFood(1);
+		snake.grow({x: canvas.width / 2, y: canvas.height / 2});
+		snake.grow({x: (canvas.width / 2) + bodySize, y: canvas.height / 2});
 		gameLoop();
 	}
 }
@@ -94,12 +100,35 @@ function	printFood() {
 	});
 }
 
-function	printSnake() {
-	ctx.fillStyle = headColor;
-	snake.body.forEach((elem) => {
-		ctx.fillRect(elem.x, elem.y, bodySize, bodySize);
+// function	printSnake() {
+// 	ctx.fillStyle = bodyColor;
+// 	ctx.strokeStyle = "white";
+// 	for (let i = snake.size - 1; i > 0; i--) {
+// 		ctx.fillRect(snake.body[i].x, snake.body[i].y, bodySize, bodySize);
+// 		ctx.strokeRect(snake.body[i].x, snake.body[i].y, bodySize, bodySize)
+// 	}
+// 	ctx.fillStyle = headColor;
+// 	ctx.fillRect(snake.head.x, snake.head.y, bodySize, bodySize);
+// }
+
+function printSnake() {
+	ctx.strokeStyle = "white";
+	ctx.lineWidth = 1;
+
+	for (let i = 1; i < snake.body.length; i++) {
 		ctx.fillStyle = bodyColor;
-	});
+		ctx.beginPath();
+		ctx.roundRect(snake.body[i].x, snake.body[i].y, bodySize, bodySize, 4);
+		ctx.fill();
+		ctx.stroke();
+	}
+	ctx.shadowColor = headColor;
+	ctx.shadowBlur = 15;
+	ctx.fillStyle = headColor;
+	ctx.beginPath();
+	ctx.roundRect(snake.head.x, snake.head.y, bodySize, bodySize, 4);
+	ctx.fill();
+	ctx.shadowBlur = 0;
 }
 
 function	clamp(num, min, max) {
@@ -110,8 +139,8 @@ function	checkCollisions(lastElem) {
 	if (checkBounds()) {
 		return true;
 	}
-	if (Math.abs(food[0].x - snake.head.x) < bodySize &&
-		Math.abs(food[0].y - snake.head.y) < bodySize) {
+	if (Math.abs(food[0].x - snake.head.x) < foodSize &&
+		Math.abs(food[0].y - snake.head.y) < foodSize) {
 			food.pop();
 			snake.grow(lastElem);
 	}
@@ -121,10 +150,10 @@ function	checkCollisions(lastElem) {
 }
 
 function	checkBounds() {
-	if (snake.head.x > (300 - bodySize) || snake.head.x <= 0)
-		return true;
-	if (snake.head.y > (300 - bodySize) || snake.head.y <= 0)
-		return true;
+	if (snake.head.x > (canvas.width - bodySize)) snake.head.x = bodySize;
+    else if (snake.head.y > (canvas.height - bodySize)) snake.head.y = bodySize;
+    else if (snake.head.x < 0) snake.head.x = canvas.width - bodySize;
+    else if (snake.head.y < 0) snake.head.y = canvas.height - bodySize;
 	return false;
 }
 
@@ -135,8 +164,8 @@ function	moveHead() {
 
 function	moveBody() {
 	for (let i = snake.size - 1; i > 0; i--) {
-		snake.body[i] = {...snake.body[i - 1]};
-	  }
+			snake.body[i] = {...snake.body[i - 1]};
+	}
 }
 
 function gameOver() {
@@ -160,10 +189,15 @@ function	drawFrame() {
 	printSnake();
 }
 
-function	gameLoop() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	updateSnake();
-	drawFrame();
+function gameLoop(currentTime) {
+	const deltaTime = currentTime - lastTime;
+
+	if (deltaTime >= targetTime) {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		updateSnake();
+		drawFrame();
+		lastTime = currentTime - (deltaTime % targetTime);
+	}
 
 	requestAnimationFrame(gameLoop);
 }
